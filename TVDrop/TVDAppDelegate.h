acb930e6ed196d6a4a45753468f64985adcab592
cameronehrlich <cameronehrlich@gmail.com>
2014-04-29 08:48:46 -0700

    Initial Commit

---
+++ TVDrop/TVDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TVDAppDelegate.h
+//  TVDrop
+//
+//  Created by Cameron Ehrlich on 4/29/14.
+//  Copyright (c) 2014 Cameron Ehrlich. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TVDAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TVDrop/TVDAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  TVDAppDelegate.m
+//  TVDrop
+//
+//  Created by Cameron Ehrlich on 4/29/14.
+//  Copyright (c) 2014 Cameron Ehrlich. All rights reserved.
+//
+
+#import "TVDAppDelegate.h"
+
+@implementation TVDAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
